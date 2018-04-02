61df54fab8797ee73eb9026fc0465d79786c77a9
melcarned <melcarned@gmail.com>
2018-04-02 17:40:04 -0400

    first commit

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -32,7 +32,7 @@ from scripts.count_ops import load_graph
 def evaluate_graph(graph_file_name):
     with load_graph(graph_file_name).as_default() as graph:
         ground_truth_input = tf.placeholder(
-            tf.float32, [None, 5], name='GroundTruthInput')
+            tf.float32, [None, 60], name='GroundTruthInput')
         
         image_buffer_input = graph.get_tensor_by_name('input:0')
         final_tensor = graph.get_tensor_by_name('final_result:0')
@@ -43,7 +43,7 @@ def evaluate_graph(graph_file_name):
             labels = ground_truth_input,
             logits = logits))
         
-    image_dir = 'tf_files/flower_photos'
+    image_dir = 'tf_files/fruits'
     testing_percentage = 10
     validation_percentage = 10
     validation_batch_size = 100
